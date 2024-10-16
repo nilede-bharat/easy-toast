0e9130e7f12e362efa4eff4f5ab4e31d0e697689
Bharat Kathore <49843578+bharatkathorer@users.noreply.github.com>
2024-10-16 17:20:21 +0530

    wip

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bharat/easy-toast',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3d0553034a75282e02d28253faad8f1d000fe979',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bharat/easy-toast' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3d0553034a75282e02d28253faad8f1d000fe979',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
